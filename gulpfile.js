ef4297cb36f4efdb722250d8ad8438bea9530dad
Son-Tung Ngo <nstungcom@gmail.com>
2019-01-18 21:09:00 +0100

    Use multiple yaml files for better readability

--- gulpfile.js
+++ gulpfile.js
@@ -26,133 +26,139 @@ const config = fs.readJsonSync('package.json',  {throws: false });
 
 // Remove the dist folder
 function clean() {
-	return fs.remove('dist/');
+  return fs.remove('dist/');
 }
 
 // Transpile css
 function css(done) {
-	return src(config.globs.scss)
-		.pipe(sourcemaps.init({ loadMaps: true }))
-		.pipe(sass.sync().on('error', sass.logError))
-		.pipe(sourcemaps.write('.', {includeContent: false, sourceRoot: '../src/scss'}))
-		.pipe(dest('dist/'));
-	done();
+  return src(config.globs.scss)
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(sourcemaps.write('.', {includeContent: false, sourceRoot: '../src/scss'}))
+    .pipe(dest('dist/'));
+  done();
 }
 // Optimize CSS
 function optimizeCSS(done) {
-	var plugins = [
-		autoprefixer(),
-		cssnano()
-	];
-	return src('dist/**/*.css')
-		.pipe(postcss(plugins))
-		.pipe(dest('dist/'));
-	done();
+  var plugins = [
+    autoprefixer(),
+    cssnano()
+  ];
+  return src('dist/**/*.css')
+    .pipe(postcss(plugins))
+    .pipe(dest('dist/'));
+  done();
 }
 
 // Transpile js
 function js(done) {
-	const bundler = browserify(config.main, { debug: true }).transform(babel);
-	return bundler.bundle()
-		.on('error', function(err) { console.error(err.stack); this.emit('end'); })
-		.pipe(source('main.js'))
-		.pipe(buffer())
-		.pipe(sourcemaps.init({ loadMaps: true }))
-		.pipe(sourcemaps.write('.', {includeContent: false, sourceRoot: '../../'}))
-		.pipe(dest('dist/js/'));
-	done();
+  const bundler = browserify(config.main, { debug: true }).transform(babel);
+  return bundler.bundle()
+    .on('error', function(err) { console.error(err.stack); this.emit('end'); })
+    .pipe(source('main.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(sourcemaps.write('.', {includeContent: false, sourceRoot: '../../'}))
+    .pipe(dest('dist/js/'));
+  done();
 }
 // Optimize JS
 function optimizeJS(done) {
-	return src('dist/**/*.js')
-		.pipe(uglify())
-		.pipe(dest('dist/'));
-	done();
+  return src('dist/**/*.js')
+    .pipe(uglify())
+    .pipe(dest('dist/'));
+  done();
 }
 
 // Transpile html
 function html(done) {
-	return src([config.globs.html, '!src/layouts/**', '!src/partials/**'])
-		.pipe(data(function(file) {
-			return yaml.safeLoad(fs.readFileSync('src/data/data.yaml', 'utf8'));
-		}))
-		.pipe(nunjucksRender({path: 'src'}))
-		.pipe(dest('dist/'));
-	done();
+  return src([config.globs.html, '!src/layouts/**', '!src/partials/**'])
+    .pipe(data(function(file) {
+      let filename = 'src/data/'+path.basename(file.basename)+'.yml';
+      try {
+        return yaml.safeLoad(fs.readFileSync(filename, 'utf8'));
+      } catch (error) {
+        console.error(error.message);
+        return {};
+      }
+    }))
+    .pipe(nunjucksRender({path: 'src'}))
+    .pipe(dest('dist/'));
+  done();
 }
 
 // Copy files
 function copyFiles(done) {
-	config.filesToCopyGlobs.notImages = '!src/images/**';
-	return src(Object.values(config.filesToCopyGlobs))
-		.pipe(dest('dist/'));
-	done();
+  config.filesToCopyGlobs.notImages = '!src/images/**';
+  return src(Object.values(config.filesToCopyGlobs))
+    .pipe(dest('dist/'));
+  done();
 }
 
 // Copy files
 function copyImages(done) {
-	return src([config.globs.images, '!src/fonts/**'])
-		.pipe(dest('dist/'));
-	done();
+  return src([config.globs.images, '!src/fonts/**'])
+    .pipe(dest('dist/'));
+  done();
 }
 // Optimize Images
 function optimizeImages(done) {
-	return src(['dist/**/*.{jpg,jpeg,png,gif,svg}', '!dist/fonts/**', '!src/fonts/**'])
-		.pipe(imagemin([
-			imageminGiflossy({interlaced: true, optimizationLevel: 3, lossy: 85}),
-			imageminMozjpeg({quality: 85}),
-			imageminPngquant({quality: 85}),
-			imagemin.svgo({plugins: [{removeViewBox: true}, {cleanupIDs: false}]})
-		]))
-		.pipe(dest('dist/'));
-	done();
+  return src(['dist/**/*.{jpg,jpeg,png,gif,svg}', '!dist/fonts/**', '!src/fonts/**'])
+    .pipe(imagemin([
+      imageminGiflossy({interlaced: true, optimizationLevel: 3, lossy: 85}),
+      imageminMozjpeg({quality: 85}),
+      imageminPngquant({quality: 85}),
+      imagemin.svgo({plugins: [{removeViewBox: true}, {cleanupIDs: false}]})
+    ]))
+    .pipe(dest('dist/'));
+  done();
 }
 
 // BrowserSync local http server
 function server(done) {
-	browsersync.init({
-		server: {baseDir: 'dist/'},
-		port: 8080,
-		open: false
-	});
-	done();
+  browsersync.init({
+    server: {baseDir: 'dist/'},
+    port: 8080,
+    open: false
+  });
+  done();
 }
 // BrowserSync live reload
 function liveReload(done) {
-	browsersync.reload();
-	done();
+  browsersync.reload();
+  done();
 }
 
 // Watch for file changes and run tasks
 function watchFiles(done) {
-	// Watch scss
-	watch(config.globs.scss, series(css, liveReload));
-	// Watch js
-	watch(config.globs.js, series(js, liveReload));
-	// Watch yaml
-	watch('src/data/data.yaml', series(html, liveReload));
-	// Watch and delete fonts
-	const watchFonts = watch(config.globs.fonts, series(copyFiles, liveReload));
-	watchFonts.on('unlink', function (filepath) {
-		const filePathFromSrc = path.relative(path.resolve('src'), filepath);
-		const destFilePath = path.resolve('dist', filePathFromSrc);	
-		fs.removeSync(destFilePath);
-	});
-	// Watch and delete html
-	const watchHTML = watch(config.globs.html, series(html, liveReload));
-	watchHTML.on('unlink', function (filepath) {
-		const filePathFromSrc = path.relative(path.resolve('src'), filepath);
-		const destFilePath = path.resolve('dist', filePathFromSrc);	
-		fs.removeSync(destFilePath);
-	});
-	// Watch and delete images
-	const watchImages = watch(config.globs.images, series(copyImages, liveReload));
-	watchImages.on('unlink', function (filepath) {
-		const filePathFromSrc = path.relative(path.resolve('src'), filepath);
-		const destFilePath = path.resolve('dist', filePathFromSrc);	
-		fs.removeSync(destFilePath);
+  // Watch scss
+  watch(config.globs.scss, series(css, liveReload));
+  // Watch js
+  watch(config.globs.js, series(js, liveReload));
+  // Watch yaml
+  watch(config.globs.yaml, series(html, liveReload));
+  // Watch and delete fonts
+  const watchFonts = watch(config.globs.fonts, series(copyFiles, liveReload));
+  watchFonts.on('unlink', function (filepath) {
+    const filePathFromSrc = path.relative(path.resolve('src'), filepath);
+    const destFilePath = path.resolve('dist', filePathFromSrc);
+    fs.removeSync(destFilePath);
   });
-	done();
+  // Watch and delete html
+  const watchHTML = watch(config.globs.html, series(html, liveReload));
+  watchHTML.on('unlink', function (filepath) {
+    const filePathFromSrc = path.relative(path.resolve('src'), filepath);
+    const destFilePath = path.resolve('dist', filePathFromSrc);
+    fs.removeSync(destFilePath);
+  });
+  // Watch and delete images
+  const watchImages = watch(config.globs.images, series(copyImages, liveReload));
+  watchImages.on('unlink', function (filepath) {
+    const filePathFromSrc = path.relative(path.resolve('src'), filepath);
+    const destFilePath = path.resolve('dist', filePathFromSrc);
+    fs.removeSync(destFilePath);
+  });
+  done();
 }
 
 // Export tasks which can be use later with "gulp taskname"
