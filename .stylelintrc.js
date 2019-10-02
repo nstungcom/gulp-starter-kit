e7e1ebf3dfd2101b85214f0e1bf3475b2acc3ec8
Son Tung Ngo <nstungcom@gmail.com>
2019-10-02 23:19:21 +0200

    Add linting for CSS, SCSS & JS

---
+++ .stylelintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: [
+    'stylelint-config-recommended-scss',
+    'stylelint-config-standard'
+  ],
+  rules: {
+    'at-rule-no-unknown': null,
+    'no-empty-source': null,
+    'number-leading-zero': 'never'
+  }
+}
