559c522fe0e3297b9e877a3110e683f1ec66e6e2
Son Tung Ngo <nstungcom@gmail.com>
2019-10-04 10:56:57 +0200

    Update linting - Remove devDependencies which are installed from peerDependencies - Add $ & jQuery as globals to .eslintrc.js - Add & set no-descending-specificity to .stylelintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,18 @@
 module.exports = {
   root: true,
+  env: {
+    'browser': true,
+    'commonjs': true,
+    'es6': true,
+    'node': true
+  },
   extends: [
-    'standard'
+    'standard',
   ],
-  rules: {}
+  globals: {
+    '$': 'readonly',
+    'jQuery': 'readonly'
+  },
+  rules: {
+  }
 }
--- .stylelintrc.js
+++ .stylelintrc.js
@@ -5,6 +5,7 @@ module.exports = {
   ],
   rules: {
     'at-rule-no-unknown': null,
+    'no-descending-specificity': null,
     'no-empty-source': null,
     'number-leading-zero': 'never'
   }
